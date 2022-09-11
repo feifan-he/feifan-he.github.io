fcb9f02c878bde26d6dcc38d33635d05ab27f99b
Feifan He <feifan@Feifans-MacBook-Pro.local>
2022-09-11 08:32:27 -0400

    update

---
+++ main/js/index.js
@@ -0,0 +1,47 @@
+
+angular.module('resumeApp', [])
+    .controller('projectsCtl', function($scope) {
+        $scope.projects = [
+            {
+                'imgs': ['./main/imgs/sort-algo/heap.png',
+                    './main/imgs/sort-algo/quick.png',
+                    './main/imgs/sort-algo/heap.png'],
+                'title': 'Card title asdf',
+                'description': 'blah..adsflk alskdfj lasdjf laksdjf laksdf lkasjdf lkasjd fl',
+                'link': 'http://feifanhe.com/sort-algo'
+            },
+            {
+                'imgs': ['./main/imgs/sort-algo/heap.png',
+                    './main/imgs/sort-algo/quick.png',
+                    './main/imgs/sort-algo/heap.png'],
+                'title': 'Card title asdf',
+                'description': 'blah..adsflk alskdfj lasdjf laksdjf laksdf lkasjdf lkasjd fl',
+                'link': 'http://feifanhe.com/sort-algo'
+            },
+            {
+                'imgs': ['./main/imgs/sort-algo/heap.png',
+                    './main/imgs/sort-algo/quick.png',
+                    './main/imgs/sort-algo/heap.png'],
+                'title': 'Card title asdf',
+                'description': 'blah..adsflk alskdfj lasdjf laksdjf laksdf lkasjdf lkasjd fl',
+                'link': 'http://feifanhe.com/sort-algo'
+            },
+            {
+                'imgs': ['./main/imgs/sort-algo/heap.png',
+                    './main/imgs/sort-algo/quick.png',
+                    './main/imgs/sort-algo/heap.png'],
+                'title': 'Card title asdf',
+                'description': 'blah..adsflk alskdfj lasdjf laksdjf laksdf lkasjdf lkasjd fl',
+                'link': 'http://feifanhe.com/sort-algo'
+            },
+            {
+                'imgs': ['./main/imgs/sort-algo/heap.png',
+                    './main/imgs/sort-algo/quick.png',
+                    './main/imgs/sort-algo/heap.png'],
+                'title': 'Card title asdf',
+                'description': 'blah..adsflk alskdfj lasdjf laksdjf laksdf lkasjdf lkasjd fl',
+                'link': 'http://feifanhe.com/sort-algo'
+            }
+        ]
+
+    });
