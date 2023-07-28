e380798d4ae98cb705d76042037bf664c462f6df
Feifan <feifan.he@mail.utoronto.ca>
2023-07-27 21:01:40 -0400

    update

--- resume/src/resumeData.js
+++ resume/src/resumeData.js
@@ -1,103 +1,108 @@
 export const projects = [
     {
-        'imgs': [
-            'imgs/employment_website/1.jpg',
-            'imgs/employment_website/2.jpg',
-            'imgs/employment_website/3.jpg',
-            'imgs/employment_website/4.jpg',
-            'imgs/employment_website/5.jpg',
-            'imgs/employment_website/6.jpg',
-            'imgs/employment_website/7.jpg',
+        imgs: [
+            "imgs/employment_website/1.jpg",
+            "imgs/employment_website/2.jpg",
+            "imgs/employment_website/3.jpg",
+            "imgs/employment_website/4.jpg",
+            "imgs/employment_website/5.jpg",
+            "imgs/employment_website/6.jpg",
+            "imgs/employment_website/7.jpg",
         ],
-        'title': 'Student Employment Website',
-        'description': 'a course-wide project designed to foster a strong community among students and potential employers. This platform enables user sign-ups and allows authenticated users to create, edit, and delete job postings and job events. Additionally, the website includes features such as a RESTful API, responsive design, and WebSocket integration for real-time chat.',
-        'tags': 'Mongoose/MongoDB, NodeJS, ExpressJS, JavaScript, React, CSS, Bootstrap, HTML, SocketIO',
+        title: "Student Employment Website",
+        description:
+            "a course-wide project designed to foster a strong community among students and potential employers. This platform enables user sign-ups and allows authenticated users to create, edit, and delete job postings and job events. Additionally, the website includes features such as a RESTful API, responsive design, and WebSocket integration for real-time chat.",
+        tags: "Mongoose/MongoDB, NodeJS, ExpressJS, JavaScript, React, CSS, Bootstrap, HTML, SocketIO",
     },
     {
-        'imgs': [
-            'imgs/umleditor/general.png',
-            'imgs/umleditor/build.png',
-            'imgs/umleditor/relationship.png',
-            'imgs/umleditor/save.png',
-            'imgs/umleditor/move.png',
-            'imgs/umleditor/zoom.png',
-            'imgs/umleditor/download.png',
+        imgs: [
+            "imgs/umleditor/general.png",
+            "imgs/umleditor/build.png",
+            "imgs/umleditor/relationship.png",
+            "imgs/umleditor/save.png",
+            "imgs/umleditor/move.png",
+            "imgs/umleditor/zoom.png",
+            "imgs/umleditor/download.png",
         ],
-        'title': 'UML Class Diagram Editor',
-        'description': 'An interactive web-based UML class diagram editor that enables users to create, edit, and delete classes and relationships.',
-        'tags': 'Mongoose/MongoDB, JavaScript, React, CSS, Bootstrap, HTML',
-        'link': 'http://feifanhe.com/umleditor/'
+        title: "UML Class Diagram Editor",
+        description:
+            "An interactive web-based UML class diagram editor that enables users to create, edit, and delete classes and relationships.",
+        tags: "Mongoose/MongoDB, JavaScript, React, CSS, Bootstrap, HTML",
+        link: "http://feifanhe.com/umleditor/",
     },
     {
-        'imgs': [
-            'imgs/keeper/keeper1.png',
-            'imgs/keeper/keeper2.png',
-        ],
-        'title': 'Note Keeper',
-        'description': 'A web-based app for storing sticky notes that allows users to create, delete and manage their notes with automatic saving.',
-        'tags': 'JavaScript, React, CSS, Bootstrap, HTML',
-        'link': 'http://feifanhe.com/keeper'
+        imgs: ["imgs/keeper/keeper1.png", "imgs/keeper/keeper2.png"],
+        title: "Note Keeper",
+        description:
+            "A web-based app for storing sticky notes that allows users to create, delete and manage their notes with automatic saving.",
+        tags: "JavaScript, React, CSS, Bootstrap, HTML",
+        link: "http://feifanhe.com/keeper",
     },
     {
-        'imgs': [
-            'imgs/sudoku/general.png',
-            'imgs/sudoku/set-easy.png',
-            'imgs/sudoku/set-hard.png',
-            'imgs/sudoku/validate.png',
-            'imgs/sudoku/win.png',
+        imgs: [
+            "imgs/sudoku/general.png",
+            "imgs/sudoku/set-easy.png",
+            "imgs/sudoku/set-hard.png",
+            "imgs/sudoku/validate.png",
+            "imgs/sudoku/win.png",
         ],
-        'title': 'Sudoku Game',
-        'description': 'An online game that generates Sudoku puzzles and allows players to choose their desired difficulty level.',
-        'tags': 'JavaScript, AngularJS, CSS, HTML',
-        'link': 'http://feifanhe.com/sudoku'
+        title: "Sudoku Game",
+        description:
+            "An online game that generates Sudoku puzzles and allows players to choose their desired difficulty level.",
+        tags: "JavaScript, AngularJS, CSS, HTML",
+        link: "http://feifanhe.com/sudoku",
     },
     {
-        'imgs': [
-            'imgs/sort-algo/quick.png',
-            'imgs/sort-algo/heap.png',
-            'imgs/sort-algo/shell.png',
+        imgs: [
+            "imgs/sort-algo/quick.png",
+            "imgs/sort-algo/heap.png",
+            "imgs/sort-algo/shell.png",
         ],
-        'title': 'In-place Sort Visualization',
-        'description': 'A web-based visualization of various in-place sort algorithms. ',
-        'tags': 'JavaScript, CSS, HTML',
-        'link': 'http://feifanhe.com/sort-algo'
+        title: "In-place Sort Visualization",
+        description:
+            "A web-based visualization of various in-place sort algorithms. ",
+        tags: "JavaScript, CSS, HTML",
+        link: "http://feifanhe.com/sort-algo",
     },
     {
-        'imgs': [
-            'imgs/traversal-algo/general.png',
-            'imgs/traversal-algo/tree-preorder.png',
-            'imgs/traversal-algo/graph-dfs.png',
-            'imgs/traversal-algo/grid-bfs.png',
+        imgs: [
+            "imgs/traversal-algo/general.png",
+            "imgs/traversal-algo/tree-preorder.png",
+            "imgs/traversal-algo/graph-dfs.png",
+            "imgs/traversal-algo/grid-bfs.png",
         ],
-        'title': 'Tree, Graph, and Grid Traversal Visualization',
-        'description': 'A web-based visualization of tree, graph, and grid traversal algorithms.',
-        'tags': 'JavaScript, CSS, HTML',
-        'link': 'http://feifanhe.com/traversal-algo'
+        title: "Tree, Graph, and Grid Traversal Visualization",
+        description:
+            "A web-based visualization of tree, graph, and grid traversal algorithms.",
+        tags: "JavaScript, CSS, HTML",
+        link: "http://feifanhe.com/traversal-algo",
     },
     {
-        'imgs': [
-            'imgs/trade-algo/general.png',
-            'imgs/trade-algo/impl1.png',
-            'imgs/trade-algo/impl2.png',
-            'imgs/trade-algo/design1.png',
-            'imgs/trade-algo/design2.png',
+        imgs: [
+            "imgs/trade-algo/general.png",
+            "imgs/trade-algo/impl1.png",
+            "imgs/trade-algo/impl2.png",
+            "imgs/trade-algo/design1.png",
+            "imgs/trade-algo/design2.png",
         ],
-        'title': 'Automated Trading via Telegram',
-        'description': 'A live trading algorithm platform with Telegram integration that runs on GCP.',
-        'tags': 'Python, Robinhood Unofficial library, Python Telegram Bot library',
-        'link': 'http://feifanhe.com/algo-trading'
+        title: "Automated Trading via Telegram",
+        description:
+            "A live trading algorithm platform with Telegram integration that runs on GCP.",
+        tags: "Python, Robinhood Unofficial library, Python Telegram Bot library",
+        link: "http://feifanhe.com/algo-trading",
     },
     {
-        'imgs': [
-            'imgs/stock-filter/output.png',
-            'imgs/stock-filter/criteria.png',
+        imgs: [
+            "imgs/stock-filter/output.png",
+            "imgs/stock-filter/criteria.png",
         ],
-        'title': 'Stock Filter',
-        'description': 'A Python-based stock filter that retrieves data from the Yahoo Finance endpoint and filters stocks using the criterias from Mark Minervini’s Pivot Volatility Contraction Pattern.',
-        'tags': 'Python, Yahoo Finance Web Endpoint',
-        'link': 'https://github.com/FEIFAN-HE/stock-filter'
-    }
-]
+        title: "Stock Filter",
+        description:
+            "A Python-based stock filter that retrieves data from the Yahoo Finance endpoint and filters stocks using the criterias from Mark Minervini’s Pivot Volatility Contraction Pattern.",
+        tags: "Python, Yahoo Finance Web Endpoint",
+        link: "https://github.com/FEIFAN-HE/stock-filter",
+    },
+];
 
 export const educationMarkdown = `
 #### Brandeis University
@@ -108,7 +113,7 @@ Bachelor of Commerce (2013  - 2017), Graduated with Distinction
 
 #### CFA Institute
 CFA® charterholder
-`
+`;
 
 export const certificationsMarkdown = `
 *Obtained certificate of completion for the following Coursera courses*
@@ -130,7 +135,7 @@ export const certificationsMarkdown = `
     - Combinatorics and Probability([Certificate](https://www.coursera.org/account/accomplishments/verify/RUKLMV7FF3PA))
     - Introduction to Graph Theory([Certificate](https://www.coursera.org/account/accomplishments/verify/5URLX94LHZLZ))
 - Introduction to Structured Query Language (SQL)([Certificate](https://www.coursera.org/account/accomplishments/verify/LMJ477ZV98JC))
-`
+`;
 
 export const languagesMarkdown = `
 
@@ -154,5 +159,4 @@ export const languagesMarkdown = `
 #### Databases
 - MySQL
 - Mongoose/MongoDB
-- InfluxDB
-`
+`;
